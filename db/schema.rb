c70141eac4a437ad5f410b17b21512f1d2616fa6
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2022-09-09 18:54:26 -0500

    stock model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_09_061045) do
+ActiveRecord::Schema.define(version: 2022_09_09_234611) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "name"
+    t.string "ticker"
+    t.decimal "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
