fe8b204cd50733f7e02be60947befb17a64d548e
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2022-09-22 22:32:58 -0500

    delete a stock tracking

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -11,4 +11,12 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was succesfully added"
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    UserStock.where(user_id: current_user.id, stock_id: stock.id).
+      first.destroy
+    flash[:notice] = "Stock #{stock.name} was succesfully removed"
+    redirect_to my_portfolio_path
+  end
 end
